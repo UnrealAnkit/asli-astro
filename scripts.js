8e236550c528186b4f540b6f6a42e4bdbdfc98ec
ANKIT KUMAR <work.ankit2@gmail.com>
2024-12-22 01:02:08 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -31,4 +31,62 @@ $(document).ready(function () {
   $("#burger-menu_close_btn").on("click", function () {
     $("#burger-menu").addClass("hidden");
   });
+
+  // Initialize testimonials slider with updated options
+  $('.testimonials-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    centerMode: true,
+    centerPadding: '0',
+    prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+    nextArrow: '<button type="button" class="slick-next">Next</button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          centerPadding: '0',
+          arrows: true,
+          dots: true
+        }
+      }
+    ]
+  });
+
+  $('.testimonials-slider .slider-container').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          arrows: true
+        }
+      }
+    ]
+  });
+
+  // Custom arrow functionality
+  $('.prev-arrow').click(function(){
+    $('.testimonials-slider .slider-container').slick('slickPrev');
+  });
+
+  $('.next-arrow').click(function(){
+    $('.testimonials-slider .slider-container').slick('slickNext');
+  });
 });
